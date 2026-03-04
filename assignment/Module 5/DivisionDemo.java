e031aed6661580223974b6337dbfd0eb0de9be62
vaishnavragavendrar <vaishnavragavendrar@gmail.com>
2026-03-04 09:44:58 +0530

    Create DivisionDemo.java

---
+++ assignment/Module 5/DivisionDemo.java
@@ -0,0 +1,14 @@
+public class DivisionDemo {
+    public static void main(String[] args) {
+        try {
+            int a = 10;
+            int b = 0;
+            int result = a / b;
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        } finally {
+            System.out.println("Finally block executed");
+        }
+    }
+}
