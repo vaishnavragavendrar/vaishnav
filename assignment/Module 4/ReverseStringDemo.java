d411298f1cc5b52fa7d1e649c870839adbb044d3
vaishnavragavendrar <vaishnavragavendrar@gmail.com>
2026-03-04 09:37:59 +0530

    Create ReverseStringDemo.java

---
+++ assignment/Module 4/ReverseStringDemo.java
@@ -0,0 +1,8 @@
+public class ReverseStringDemo {
+    public static void main(String[] args) {
+        String str = "Java Programming";
+        StringBuilder sb = new StringBuilder(str);
+        sb.reverse();
+        System.out.println("Reversed String: " + sb);
+    }
+}
