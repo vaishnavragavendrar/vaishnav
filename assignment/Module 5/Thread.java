6ef5fcb88dead0ac2277a5bbd84d58fcb4db6b3d
vaishnavragavendrar <vaishnavragavendrar@gmail.com>
2026-03-04 09:47:54 +0530

    Create Thread.java

---
+++ assignment/Module 5/Thread.java
@@ -0,0 +1,21 @@
+class MyThread extends Thread {
+    public void run() {
+        System.out.println("Thread created using Thread class");
+    }
+}
+
+class MyRunnable implements Runnable {
+    public void run() {
+        System.out.println("Thread created using Runnable interface");
+    }
+}
+
+public class Thread {
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+        t1.start();
+
+        Thread t2 = new Thread(new MyRunnable());
+        t2.start();
+    }
+}
