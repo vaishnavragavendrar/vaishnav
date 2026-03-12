75c7cf209b9220a9cb13944261f9a7864d0a7d8d
vaishnavragavendrar <vaishnavragavendrar@gmail.com>
2026-03-12 09:08:36 +0530

    Create MajorityElement.java

---
+++ assignment/module 6/MajorityElement.java
@@ -0,0 +1,25 @@
+package assignment3;
+
+public class MajorityElement {
+
+    public static void main(String[] args) {
+
+        int arr[] = {2,2,1,1,2,2,2};
+
+        int count = 0;
+        int candidate = 0;
+
+        for(int num : arr){
+
+            if(count == 0)
+                candidate = num;
+
+            if(num == candidate)
+                count++;
+            else
+                count--;
+        }
+
+        System.out.println("Majority Element = " + candidate);
+    }
+}
