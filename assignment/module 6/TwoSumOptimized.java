29a7969c3c1e2d10731cef9a93fafc472e242d9c
vaishnavragavendrar <vaishnavragavendrar@gmail.com>
2026-03-12 09:11:40 +0530

    Create TwoSumOptimized.java

---
+++ assignment/module 6/TwoSumOptimized.java
@@ -0,0 +1,25 @@
+package assignment3;
+
+import java.util.HashMap;
+
+public class TwoSumOptimized {
+
+    public static void main(String[] args) {
+
+        int arr[] = {2,7,11,15};
+        int target = 9;
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i=0;i<arr.length;i++){
+
+            int complement = target - arr[i];
+
+            if(map.containsKey(complement)){
+                System.out.println("Indices: " + map.get(complement) + " " + i);
+            }
+
+            map.put(arr[i],i);
+        }
+    }
+}
