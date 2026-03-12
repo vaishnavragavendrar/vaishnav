b14f6fd4433ee5e049adf63ecee612200c320795
vaishnavragavendrar <vaishnavragavendrar@gmail.com>
2026-03-12 09:13:15 +0530

    Create BubbleSort.java

---
+++ assignment/module 7/BubbleSort.java
@@ -0,0 +1,24 @@
+package assignment3;
+
+public class BubbleSort {
+
+    public static void main(String[] args){
+
+        int arr[]={5,3,8,4,2};
+
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+
+                if(arr[j]>arr[j+1]){
+
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+
+        for(int x:arr)
+            System.out.print(x+" ");
+    }
+}
